7491a8fd3bf5e4552f9df713b0cc87a5dc4acace
Andrewravd <82881494+Andrewravd@users.noreply.github.com>
2023-02-12 02:51:52 +0300

    Написал ui тесты

--- src/test/java/TestSignInWithButtonConfirm.java
+++ src/test/java/TestSignInWithButtonConfirm.java
@@ -1,23 +1,5 @@
-import static com.codeborne.selenide.Selenide.open;
-
 public class TestSignInWithButtonConfirm {
 
 
-    @Test
-    @Description("Авторизация через кнопку <Войти> в форме регистрации")
-    public void signInSignInWithButtonConfirm() {
-        steps.register(user, registrationPage.getName(),
-                registrationPage.getEmail(), registrationPage.getPassword());
-        steps.clickOnElement(registrationPage.getButtonConfirm());
-        steps.checkUrl(Urls.LOGIN_URL);
-        open(Urls.REGISTRATION_URL);
-        steps.checkUrl(Urls.REGISTRATION_URL);
-        steps.clickOnElement(registrationPage.getLinkSignIn());
-        steps.login(user, loginPage.getEmail(), loginPage.getPassword(), loginPage.getButtonSignIn());
-        steps.clickOnElement(loginPage.getButtonSignIn());
-        steps.checkUrl(Urls.MAIN_PAGE_URL);
-        steps.clickOnElement(mainPage.getaAccount());
-        steps.checkUrl(Urls.PROFILE_URL);
-    }
 
 }
--- src/test/java/TestSignInWithButtonLogin.java
+++ src/test/java/TestSignInWithButtonLogin.java
@@ -1,20 +1,3 @@
 public class TestSignInWithButtonLogin {
 
-    @Test
-    @Description("Авторизация по кнопке <Войти в аккаунт> на главной")
-    public void signInWithButtonLogin() {
-        steps.clickOnElement(mainPage.getButtonLogin());
-        steps.checkUrl(Urls.LOGIN_URL);
-        steps.clickOnElement(loginPage.getLinkRegister());
-        steps.register(user, registrationPage.getName(),
-                registrationPage.getEmail(), registrationPage.getPassword());
-        steps.clickOnElement(registrationPage.getButtonConfirm());
-        steps.checkUrl(Urls.LOGIN_URL);
-        steps.login(user, loginPage.getEmail(), loginPage.getPassword(), loginPage.getButtonSignIn());
-        steps.clickOnElement(loginPage.getButtonSignIn());
-        steps.checkUrl(Urls.MAIN_PAGE_URL);
-        steps.clickOnElement(mainPage.getaAccount());
-        steps.checkUrl(Urls.PROFILE_URL);
-    }
-
 }
