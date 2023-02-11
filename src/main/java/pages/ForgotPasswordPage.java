1f1bbda272c2faeb6dda78872a87e5ff2e0f2a3f
Andrewravd <82881494+Andrewravd@users.noreply.github.com>
2023-02-12 02:24:54 +0300

    Написал ui тесты

--- src/main/java/pages/ForgotPasswordPage.java
+++ src/main/java/pages/ForgotPasswordPage.java
@@ -1,18 +1,3 @@
-package pages;
-
-import com.codeborne.selenide.SelenideElement;
-import org.openqa.selenium.By;
-
-import static com.codeborne.selenide.Selenide.$;
-
 public class ForgotPasswordPage {
-    private SelenideElement linkSignIn = $(By.xpath(".//a[text()='Войти']"));
 
-    public SelenideElement getLinkSignIn() {
-        return linkSignIn;
-    }
-
-    public void setgetLinkSignIn(SelenideElement linkSignIn) {
-        this.linkSignIn = linkSignIn;
-    }
 }
