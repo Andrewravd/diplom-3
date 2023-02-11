9e126e13186418ee93c2ac4a6a5dc1232f54a162
Andrewravd <82881494+Andrewravd@users.noreply.github.com>
2023-02-12 02:56:48 +0300

    Написал ui тесты

--- src/test/java/TestSignInWithLinkAccount.java
+++ src/test/java/TestSignInWithLinkAccount.java
@@ -1,3 +1,33 @@
 public class TestSignInWithLinkAccount {
 
+    @Before
+    public void setUp() {
+        user = UserGenerator.getDefaultUser();
+        steps = new Steps();
+        mainPage = new MainPage();
+        registrationPage = new RegistrationPage();
+        loginPage = new LoginPage();
+        open(Urls.MAIN_PAGE_URL);
+    }
+    @After
+    public void closeWebDriver() {
+        Selenide.closeWebDriver();
+    }
+    @Test
+    @Description("Авторизация по кнопке <Личный кабинет>")
+    public void signIn() {
+        steps.clickOnElement(mainPage.getaAccount());
+        steps.checkUrl(Urls.LOGIN_URL);
+        steps.clickOnElement(loginPage.getLinkRegister());
+        steps.register(user, registrationPage.getName(),
+                registrationPage.getEmail(), registrationPage.getPassword());
+        steps.clickOnElement(registrationPage.getButtonConfirm());
+        steps.checkUrl(Urls.LOGIN_URL);
+        steps.login(user, loginPage.getEmail(), loginPage.getPassword(), loginPage.getButtonSignIn());
+        steps.clickOnElement(loginPage.getButtonSignIn());
+        steps.checkUrl(Urls.MAIN_PAGE_URL);
+        steps.clickOnElement(mainPage.getaAccount());
+        steps.checkUrl(Urls.PROFILE_URL);
+    }
+
 }
