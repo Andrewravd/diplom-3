3f32e2ce448d4d6bbd48a4360bcc7ecb0af8ccea
Andrewravd <82881494+Andrewravd@users.noreply.github.com>
2023-02-12 03:07:28 +0300

    добавил тесты

--- src/main/java/UserGenerator.java
+++ src/main/java/UserGenerator.java
@@ -1,3 +1,11 @@
 public class UserGenerator {
+    public static User getDefaultUser() {
+        return new User(DataGenerator.getRandomString().toLowerCase(), DataGenerator.getRandomEmail().toLowerCase(),
+                DataGenerator.getRandomString().toLowerCase());
+    }
 
+    public static User getUserWitIncorrectPassword() {
+        return new User(DataGenerator.getRandomString(), DataGenerator.getRandomEmail(),
+                DataGenerator.getRandomStringPhone());
+    }
 }
